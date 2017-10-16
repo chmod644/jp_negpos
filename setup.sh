af4a2441ac3d44c9d111a44bfc7baf047d8e1fee
someone <someone@example.com>
2017-10-16 19:01:58 +0900

    Add download script for japanese neg-pos dictionary

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir jp_negpos_dict
+wget http://www.cl.ecei.tohoku.ac.jp/resources/sent_lex/wago.121808.pn -O jp_negpos_dict/wago.121808.pn
+wget http://www.cl.ecei.tohoku.ac.jp/resources/sent_lex/pn.csv.m3.120408.trim -O jp_negpos_dict/pn.csv.m3.120408.trim
